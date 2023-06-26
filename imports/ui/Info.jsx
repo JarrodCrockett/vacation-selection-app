240ed8d7d68bedde1694291ff0224e71f6e717d2
Matthew Lowe <matthew.ryan.lowe@gmail.com>
2023-06-26 09:46:55 -0500

    Revert "initial commit"
    This reverts commit 3cb8e330df3eaf0c8a54f2dc544c44c45609f081.

--- imports/ui/Info.jsx
+++ imports/ui/Info.jsx
@@ -5,10 +5,7 @@ import Stack from "@mui/material/Stack";
 const Info = () => {
   return (
     <div>
-      <p>Oftentimes when checking local RFPs it can become not only a tedious process,
-        but also one that takes away from internal development time. So we thought,
-        wouldn't it be wonderful if we implemented a machine learning model to check RFPs
-        for us instead??? Enter RFP Checker.</p>
+      <p>Let me tell you about this thing.</p>
       <p>It is a handy thing.</p>
       {/* todo: add something nice for handling code snippets */}
       <Stack spacing={2}>
